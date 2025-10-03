876d5ea94c5e3dca661302a0aa033e4399263dfb
Dima <skylne162@gmail.com>
2025-10-03 13:02:39 +0300

    rework

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,7 +1,7 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 const gallery = document.querySelector(".gallery");
-const loader = document.querySelector(".loader-container");
+const loader = document.querySelector(".loader");
 
 
 export function createGallery(images) {
@@ -44,9 +44,9 @@ export function clearGallery() {
 };
 
 export function showLoader() {
-    loader.insertAdjacentHTML("afterbegin", `<span class="loader"></span>`);
+  loader.classList.remove('hidden');
 };
 
 export function hideLoader() {
-    loader.innerHTML = "";
+    loader.classList.add('hidden');
 };
